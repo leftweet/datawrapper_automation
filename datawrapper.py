db31d2f14816a0c8e493dfb0885239658aaadc98
leftweet <jtbeilby@gmail.com>
2025-04-29 18:55:48 -0700

    Update datawrapper.py

--- datawrapper.py
+++ datawrapper.py
@@ -378,7 +378,7 @@ def create_and_publish_datawrapper_chart(df, team1_abbr, team2_abbr):
             st.subheader("Datawrapper Game Flow Chart")
             # Construct the basic iframe HTML using an f-string with white background style
             basic_iframe_html = f"""
-<iframe title="{chart_title}" aria-label="Interactive line chart" id="datawrapper-chart-{chart_id}" src="https://datawrapper.dwcdn.net/{chart_id}/1/" scrolling="no" frameborder="0" style="width: 100%; border: none; background-color: white;" height="400" data-external="1"></iframe>
+<iframe title="{chart_title}" aria-label="Interactive line chart" id="datawrapper-chart-{chart_id}" src="https://datawrapper.dwcdn.net/{chart_id}/1/" scrolling="no" frameborder="0" style="width: 100%; border: none; background-color: white;" height="500" data-external="1"></iframe>
 """
             # Use components.html to embed the iframe
             components.html(basic_iframe_html, height=600)
