9154360f8f4e124e003074d6edb4de444d15e289
leftweet <jtbeilby@gmail.com>
2025-04-29 19:55:34 -0700

    Update datawrapper.py

--- datawrapper.py
+++ datawrapper.py
@@ -265,6 +265,7 @@ def create_and_publish_datawrapper_chart(df, team1_abbr, team2_abbr):
                     "source-url": "", # Consider adding the original_url here if possible
                     "intro": f"Game flow chart for {team_a_col} vs. {team_b_col}.",
                     "byline": "",
+                    "get-the-data": false,
                     "aria-description": f"Line chart showing the score progression for {team_a_col} and {team_b_col} throughout the game.",
                     "number-format": "-",
                     "number-divisor": 0,
